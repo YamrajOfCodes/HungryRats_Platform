6065cff4a77b3b4fdbff655b9d4d5e206c284c7a
YamrajOfcodes <kundanpatil0111@.gmail.com>
2025-02-28 15:58:11 +0530

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,19 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        webbg: "#fcefd1db",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+  ],
 } satisfies Config;

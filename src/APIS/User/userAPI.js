53d195d0207fe057b3be6b2296938d4b6e5cc3d5
YamrajOfcodes <kundanpatil0111@.gmail.com>
2025-04-02 11:25:39 +0530

    changes in admin side

--- src/APIS/User/userAPI.js
+++ src/APIS/User/userAPI.js
@@ -2,34 +2,34 @@ import { commonrequest } from "../commonrequest"
 import { BASE_URL } from "../helper"
 
 
-export const userRegisterAPI = async(data,header)=>{
-    return await commonrequest("POST",`${BASE_URL}/user/api/register`,data,header,"");
+export const userRegisterAPI = async (data, header) => {
+    return await commonrequest("POST", `${BASE_URL}/user/api/register`, data, header, "");
 }
 
-export const userLoginAPI = async(data,header)=>{
-    return await commonrequest("POST",`${BASE_URL}/user/api/login`,data,header,"");
+export const userLoginAPI = async (data, header) => {
+    return await commonrequest("POST", `${BASE_URL}/user/api/login`, data, header, "");
 }
 
-export const userverifyAPI = async(data,header)=>{
-    return await commonrequest("GET",`${BASE_URL}/user/api/userverify`,data,header,"user");
-}
-
-
-export const userLogoutAPI = async(data,header)=>{
-    return await commonrequest("POST",`${BASE_URL}/user/api/logout`,{},header,"user")
-}
-
-export const addproductAPI = async(data,header)=>{
-    return await commonrequest("POST",`${BASE_URL}/product/api/addproduct`,data,header,"");
+export const userverifyAPI = async (data, header) => {
+    return await commonrequest("GET", `${BASE_URL}/user/api/userverify`, data, header, "user");
 }
 
 
-export const getallproductsAPI = async(data,header)=>{
-    return await commonrequest("GET",`${BASE_URL}/product/api/getallproducts`,"",header,"");
+export const userLogoutAPI = async (data, header) => {
+    return await commonrequest("POST", `${BASE_URL}/user/api/logout`, {}, header, "user")
 }
 
-export const deleteproductAPI = async(data,header)=>{
-    return await commonrequest("DELETE",`${BASE_URL}/product/api/deleteproduct/${data}`,{},header,"");
+export const addproductAPI = async (data, header) => {
+    return await commonrequest("POST", `${BASE_URL}/product/api/addproduct`, data, header, "");
+}
+
+
+export const getallproductsAPI = async (data, header) => {
+    return await commonrequest("GET", `${BASE_URL}/product/api/getallproducts`, "", header, "");
+}
+
+export const deleteproductAPI = async (data, header) => {
+    return await commonrequest("DELETE", `${BASE_URL}/product/api/deleteproduct/${data}`, {}, header, "");
 }
 
 
@@ -37,35 +37,35 @@ export const deleteproductAPI = async(data,header)=>{
 // cartAPI
 
 
-export const addtocartAPI = async(data,header)=>{
-    return await commonrequest("POST",`${BASE_URL}/user/api/addcart`,data,header,"");
+export const addtocartAPI = async (data, header) => {
+    return await commonrequest("POST", `${BASE_URL}/user/api/addcart`, data, header, "");
 }
 
-export const getcartAPI = async(data,header)=>{
+export const getcartAPI = async (data, header) => {
     // console.log(data);
-    
-    return await commonrequest("GET",`${BASE_URL}/user/api/getcart/${data}`,data,header,"user");
+
+    return await commonrequest("GET", `${BASE_URL}/user/api/getcart/${data}`, data, header, "user");
 }
 
-export const deletecartAPI = async(data,header)=>{
-    return await commonrequest("DELETE",`${BASE_URL}/user/api/deletecart/${data}`,{},header,"");
+export const deletecartAPI = async (data, header) => {
+    return await commonrequest("DELETE", `${BASE_URL}/user/api/deletecart/${data}`, {}, header, "");
 }
 
 
 
 // payment and order API
 
-export const checksubscriptionAPI =  async(data,header)=>{
-    return await commonrequest("GET",`${BASE_URL}/user/api/subscription/${data}`,"",header,"");
+export const checksubscriptionAPI = async (data, header) => {
+    return await commonrequest("GET", `${BASE_URL}/user/api/subscription/${data}`, "", header, "");
 }
 
-export const getsubscriptionAPI =  async(data,header)=>{
-    return await commonrequest("POST",`${BASE_URL}/user/api/subscribe`,data,header,"");
+export const getsubscriptionAPI = async (data, header) => {
+    return await commonrequest("POST", `${BASE_URL}/user/api/subscribe`, data, header, "");
 }
 
 
-export const postorderAPI =  async(data,header)=>{
-    return await commonrequest("POST",`${BASE_URL}/product/api/orders/${data.id}`,data,header,"");
+export const postorderAPI = async (data, header) => {
+    return await commonrequest("POST", `${BASE_URL}/product/api/orders/${data.id}`, data, header, "");
 }
 
 
@@ -74,8 +74,8 @@ export const postorderAPI =  async(data,header)=>{
 
 // messgaeAPI
 
-export const contactAPI = async(data,header)=>{
-    return await commonrequest("POST",`${BASE_URL}/user/api/message`,data,header,"");
+export const contactAPI = async (data, header) => {
+    return await commonrequest("POST", `${BASE_URL}/user/api/message`, data, header, "");
 }
 
 
@@ -84,24 +84,22 @@ export const contactAPI = async(data,header)=>{
 
 //Admin API
 
-export const getallCustomersAPI = async(data,header)=>{
-    return await commonrequest("GET",`${BASE_URL}/admin/api/getallusers`,"",header,"");
+export const getallCustomersAPI = async (data, header) => {
+    return await commonrequest("GET", `${BASE_URL}/admin/api/getallusers`, "", header, "");
 }
 
-export const getordersAPI = async(data,header)=>{
-    return await commonrequest("GET",`${BASE_URL}/admin/api/getorders`,"",header,"");
+export const getordersAPI = async (data, header) => {
+    return await commonrequest("GET", `${BASE_URL}/admin/api/getorders`, "", header, "");
 }
 
 
 
-export const deleteuserAPI = async(data,header)=>{
-    return await commonrequest("DELETE",`${BASE_URL}/admin/api/deleteuser/${data}`,{},header,"");
+export const deleteuserAPI = async (data, header) => {
+    return await commonrequest("DELETE", `${BASE_URL}/admin/api/deleteuser/${data}`, {}, header, "");
 }
 
-export const deleteorderAPI = async(data,header)=>{
-    return await commonrequest("DELETE",`${BASE_URL}/admin/api/deleteorder/
-        
-        ${data}`,{},header,"");
+export const deleteorderAPI = async (data, header) => {
+    return await commonrequest("DELETE", `${BASE_URL}/admin/api/deleteorder/${data}`, {}, header, "");
 }
 
 
